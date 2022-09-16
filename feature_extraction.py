773fa21dd08fbe312ef1b139ff5705719938ace1
Jason Liartis <g.liartis@gmail.com>
2022-09-16 16:40:18 +0300

    moved data to cuda

--- feature_extraction.py
+++ feature_extraction.py
@@ -25,7 +25,8 @@ net.load_state_dict(checkpoint['model_state'])
 net.eval()
 
 data = Artists(args.base_path, args.ids_fn, args.images_dir, train=False)
-features = [(net.avgpool(net.features(x.cuda().unsqueeze(0))).flatten().cpu().numpy(), y) for x, y in data]
+features = [(net.avgpool(net.features(x.cuda().unsqueeze(0))).flatten().cpu().detach().numpy(), y)
+            for x, y in data]
 
 with open(args.features_fn, 'wb') as fp:
     pickle.dump(features, fp)
